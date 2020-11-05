5b6e25752a1b5591205c511b1b3cfb1e369ea037
Jerome <jlau122301@gmail.com>
2020-11-05 13:59:46 -0800

    User Sign up (teacher or student)

--- app/src/main/java/com/themusicians/musiclms/MainActivity.java
+++ app/src/main/java/com/themusicians/musiclms/MainActivity.java
@@ -3,9 +3,12 @@ package com.themusicians.musiclms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void teacherSignup(View view) {
+        Intent nextPageT = new Intent(this, signup.class);
+        startActivity(nextPageT);
+    }
+
+    public void studentSignup(View view) {
+        Intent nextPageS = new Intent(this, signup.class);
+        startActivity(nextPageS);
+    }
 }
--- app/src/main/java/com/themusicians/musiclms/signup.java
+++ app/src/main/java/com/themusicians/musiclms/signup.java
@@ -3,9 +3,11 @@ package com.themusicians.musiclms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
