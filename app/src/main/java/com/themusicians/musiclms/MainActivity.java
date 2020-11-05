0fdeb448853947280115c11bd69bade2b36c5ef0
Harveer <harveerk@sfu.ca>
2020-11-05 14:37:31 -0800

    making login clickable

--- app/src/main/java/com/themusicians/musiclms/MainActivity.java
+++ app/src/main/java/com/themusicians/musiclms/MainActivity.java
@@ -1,11 +1,18 @@
 package com.themusicians.musiclms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.widget.EditText;
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.message";
@@ -25,4 +32,21 @@ public class MainActivity extends AppCompatActivity {
         Intent nextPageS = new Intent(this, signup.class);
         startActivity(nextPageS);
     }
+    public void orLogin(View view) {
+        TextView textview = findViewById(R.id.Or_login);
+        String text = "or Login";
+        SpannableString ss1 = new SpannableString(text);
+
+        ClickableSpan clickablespanLogin = new ClickableSpan(){
+            @Override
+            public void onClick(@NonNull View view) {
+                Toast.makeText(MainActivity.this,"one",Toast.LENGTH_SHORT);
+            }
+        };
+        ss1.setSpan(clickablespanLogin,3,7, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textview.setText(ss1);
+        textview.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
 }
