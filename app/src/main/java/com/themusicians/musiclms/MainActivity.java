e84e46f6b3b79cb6691d31dab9221b5b934dd3fc
mingyangwei <xavier012428@gmail.com>
2020-11-07 00:18:34 -0800

    update activity_assignment_overview.xml

--- app/src/main/java/com/themusicians/musiclms/MainActivity.java
+++ app/src/main/java/com/themusicians/musiclms/MainActivity.java
@@ -8,6 +8,7 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.view.View;
@@ -18,10 +19,24 @@ import com.themusicians.musiclms.nodeForms.AssignmentCreateFormActivity;
 import com.themusicians.musiclms.nodeViews.AssignmentOverviewActivity;
 import com.themusicians.musiclms.ui.login.signin;
 
+import java.sql.BatchUpdateException;
+
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        button = (Button) findViewById(R.id.assignment_overview_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity_assignment_create_form();
+            }
+        });
 
         // Temporary to test Assignment Page
         Intent redirectToAssignment = new Intent(this, AssignmentOverviewActivity.class);
@@ -44,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void openActivity_assignment_create_form(){
+        Intent intent = new Intent(this,AssignmentCreateFormActivity.class);
+        startActivity(intent);
     }
 
     public void teacherSignup(View view) {
