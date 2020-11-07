c03241ef9cb3399eb6f76f16d27ca820586b1804
Chris-Happy7 <nathantsai28@gmail.com>
2020-11-06 20:57:38 -0800

    Add fragment.

--- app/src/main/java/com/themusicians/musiclms/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/themusicians/musiclms/ui/gallery/GalleryFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.themusicians.musiclms.R;
 
 public class GalleryFragment extends Fragment {
