cb22c94a618f6d654092767e71c6bdfcc54c1476
Shifan He <caronhsf@gmail.com>
2020-11-13 00:43:52 +0800

    Chat UI

--- app/src/main/java/com/themusicians/musiclms/Chat.java
+++ app/src/main/java/com/themusicians/musiclms/Chat.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
+    Button Return;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,10 @@ public class Chat extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
 
-    //    public void ReturnMain(View view) {
-    //        Intent MainPage = new Intent(this, MainActivity.class);
-    //       startActivity(MainPage);
-    //   }
+       /* public void ReturnMain(View view) {
+            Intent MainPage = new Intent(this, MainActivity.class);
+           startActivity(MainPage);
+       }*/
 
     }
 }
