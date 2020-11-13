a4b3be998d086d982cbcd98edb47d04ae3dc34b5
Shifan He <caronhsf@gmail.com>
2020-11-13 13:19:45 +0800

    Chat code

--- app/src/main/java/com/themusicians/musiclms/Chat.java
+++ app/src/main/java/com/themusicians/musiclms/Chat.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Chat extends AppCompatActivity {
     Button Return;
@@ -15,6 +18,20 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                    "Welcome " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+
+            // Load chat room contents
+            displayChatMessages();
+    }
+    private void displayChatMessages() {
 
     }
 }
