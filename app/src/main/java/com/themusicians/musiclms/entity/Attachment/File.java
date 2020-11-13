d9d9586d2fb6095210a05dbb8ad1ec34f25e72dc
Chris-Happy7 <nathantsai28@gmail.com>
2020-11-12 21:53:07 -0800

    Add File attachment class.

---
+++ app/src/main/java/com/themusicians/musiclms/entity/Attachment/File.java
@@ -0,0 +1,48 @@
+package com.themusicians.musiclms.entity.Attachment;
+
+/**
+ * The File Attachment class
+ *
+ * @contributor Mingyang Wei
+ * @author Nathan Tsai
+ * @since Nov 12, 2020
+ */
+public class File extends Attachment {
+
+  protected final String type = "file";
+
+  /**
+   * Where the file is stored
+   */
+  protected String fileId;
+
+  /**
+   * Default constructor
+   */
+  public File() {
+    super();
+  }
+
+  /**
+   * Save the file to database + save the file to FireStore
+   * @return Whether the entity was saved or not
+   */
+  @Override
+  public boolean save() {
+    super.save();
+    // Save the file here
+
+    return true;
+  }
+
+  /**
+   * The setters and getters for where the file is stored
+   */
+  public String getFileId() {
+    return fileId;
+  }
+
+  public void setFileId(String fileId) {
+    this.fileId = fileId;
+  }
+}
