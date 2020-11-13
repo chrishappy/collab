3a3b5e9bfc3e5396b0ec2ebf95563f9e6892c9df
Jerome <jlau122301@gmail.com>
2020-11-12 23:10:03 -0800

    User profile fetch from firebase display name

--- app/src/main/java/com/themusicians/musiclms/userProfile.java
+++ app/src/main/java/com/themusicians/musiclms/userProfile.java
@@ -1,22 +1,58 @@
 package com.themusicians.musiclms;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 /**
+ *
  * ....
  *
- * <p>Contributors: Jerome Lau Created by Jerome Lau on 2020-11-06
+ * Contributors: Jerome Lau
+ * Created by Jerome Lau on 2020-11-06
  *
- * <p>--------------------------------
+ * --------------------------------
  *
  * @todo View user info
  */
+
 public class userProfile extends AppCompatActivity {
+
+  protected TextView myName;
+  protected FirebaseUser currentUser;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_user_profile);
+
+    myName = findViewById(R.id.myName);
+
+    currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("node__user").child(currentUser.getUid());
+    reference.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(@NonNull DataSnapshot snapshot) {
+        String name = snapshot.child("name").getValue().toString();
+
+        myName.setText(name);
+      }
+
+      @Override
+      public void onCancelled(@NonNull DatabaseError error) {
+
+      }
+    });
   }
 }
