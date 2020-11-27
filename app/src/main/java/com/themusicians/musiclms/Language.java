02da77bb2f615aa9a8abea05b1a131d797d5669a
Shifan He <caronhsf@gmail.com>
2020-11-27 13:37:42 +0800

    Translation

--- app/src/main/java/com/themusicians/musiclms/Language.java
+++ app/src/main/java/com/themusicians/musiclms/Language.java
@@ -1,12 +1,18 @@
 package com.themusicians.musiclms;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.Button;
 import android.widget.Toast;
 import android.content.Intent;
 import android.view.View;
@@ -20,21 +26,59 @@ public class Language extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setApplocale("zh-rCN");
-
+        loadLocale();
         setContentView(R.layout.activity_language);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getResources().getString(R.string.app_name));
+
+        Button changeLang = findViewById(R.id.changeMyLang);
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangeLanguageDialog();
+            }
+        });
     }
 
-    private void setApplocale(String localeCode) {
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration conf = res.getConfiguration();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            conf.setLocale(new Locale(localeCode.toLowerCase()));
-        } else {
-            conf.locale = new Locale(localeCode.toLowerCase());
-        }
-        res.updateConfiguration(conf, dm);
+    private void showChangeLanguageDialog() {
+        final String[] listItems = {"简体中文", "English"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(Language.this);
+        mBuilder.setTitle("Choose Language...");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0){
+                    setLocale ("en");
+                    recreate();
+                }
+                else if (i == 1){
+                    setLocale ("zh-rCN");
+                    recreate();
+                }
+
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
     }
 }
