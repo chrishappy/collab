a3d4ba7b770e9b327c3fb7346c757501678f0be6
Harveer <harveerk@sfu.ca>
2020-11-26 19:23:30 -0800

    notifications

---
+++ app/src/main/java/com/themusicians/musiclms/Notifications/Client.java
@@ -0,0 +1,16 @@
+package com.themusicians.musiclms.Notifications;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Client {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String url){
+        if(retrofit == null){
+            retrofit = new Retrofit.builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/themusicians/musiclms/Notifications/MyResponse.java
@@ -0,0 +1,6 @@
+package com.themusicians.musiclms.Notifications;
+
+public class MyResponse {
+
+    public int success;
+}
